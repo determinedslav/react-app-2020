7a06a33bb9817f39c660030e9add088911c0d97b
Determined Slav <slavimitev10@gmail.com>
2020-01-25 16:40:59 +0200

    counts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,36 @@ import QuantityInput from '../components/QuantityInput';
 
 class Home extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            counts: [1,2,3,4,5]
+        }
+    }
+
+    changeCounts = () => {
+        this.setState({
+            counts : [8,2],
+        })
+    }
+
+    getQuantityInputs = () => {
+        const counts = this.state.counts;
+
+        const inputs = counts.map(element => {
+            return <QuantityInput key={element} count={element}/>
+        })
+
+        return inputs;
+    }
 
     render(){
-        return <div>
-            <QuantityInput count={5}/>
+        return (
+        <div>
+            <button onClick={this.changeCounts} type="button" className="btn btn-primary mb-3">Click</button>
+            {this.getQuantityInputs()}
         </div>
+        )
     }
 }
 
