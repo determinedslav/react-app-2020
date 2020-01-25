f66583e34008dfff19c145c09b976759e05896b7
Determined Slav <slavimitev10@gmail.com>
2020-01-25 15:41:22 +0200

    quantity input

--- src/components/QuantityInput.js
+++ src/components/QuantityInput.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const QuantityInput = props => {
+
+    const [count, setCount] = useState(props.count);
+
+    const getQuantityMessage = count => {
+        if (count > 7) {
+            return (
+                <div class="alert alert-success">
+                    Satisfactory condition
+                </div>
+            ) 
+        }
+        if (count < 3) {
+            return (
+                <div class="alert alert-danger">
+                    Imminant danger
+                </div>
+            ) 
+        }
+        return (
+            <div class="alert alert-warning">
+                Uncertant state
+            </div>
+        ) 
+    }
+
     return(
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <button class="btn btn-outline-danger" type="button"><i className = "fa fa-minus-circle"></i></button>
+        <div>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <button onClick={() => setCount(count - 1) } className="btn btn-outline-danger" type="button"><i className = "fa fa-minus-circle"></i></button>
+                </div>
+                <input readOnly value={count} type="text" className="form-control text-center" aria-label="" aria-describedby="basic-addon1"/>
+                <div className="input-group-append">
+                    <button onClick={() => setCount(count + 1) } className="btn btn-outline-success" type="button"><i className = "fa fa-plus-circle"></i></button>
+               </div>
             </div>
-            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1"/>
-            <div class="input-group-append">
-                <button class="btn btn-outline-success" type="button"><i className = "fa fa-plus-circle"></i></button>
-            </div>
+
+            {getQuantityMessage(count)}  
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component{
 
     render(){
         return <div>
-            <QuantityInput/>
+            <QuantityInput count={5}/>
         </div>
     }
 }
